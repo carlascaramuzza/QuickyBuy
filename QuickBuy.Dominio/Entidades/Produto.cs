5bdb7c2e56fc45fb6818776f742aeb815a6cf02c
Carla Rodrigues <carla.rodrigues@programmers.com.br>
2019-05-16 08:50:56 -0300

    criação2

---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace QuickBuy.Dominio.Entidades
+{
+   public class Produto
+    {
+        public int Id { get; set; }
+        public string Nomel { get; set; }
+        public string Descricao { get; set; }
+        public string Preco { get; set; }
+    
+    }
+}
---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QuickBuy.Dominio.Enumerados;
+
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao  { get; set; }
+        public Boolean EhBoleto => Id = TipoFormaPagamentoEnum.Boleto;
+
+    }
+}
